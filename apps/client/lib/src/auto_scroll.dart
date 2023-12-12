d854d490e36c1326f3e70f2f230d7cc3ea397316
Melvin <melvinnnamani@gmail.com>
2023-12-12 14:25:46 +0100

    add auto_scroll_child widget to allow the screen scroll instead of overflow

---
+++ apps/client/lib/src/auto_scroll.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class AutoScrollChild extends StatelessWidget {
+  final Widget child;
+  final ScrollController? controller;
+  const AutoScrollChild({
+    super.key,
+    required this.child,
+    this.controller,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(
+      builder: (BuildContext context, BoxConstraints constraints) {
+        return SingleChildScrollView(
+          physics: const AlwaysScrollableScrollPhysics(),
+          controller: controller,
+          child: Container(
+            padding: EdgeInsets.only(
+                bottom: MediaQuery.of(context).viewInsets.bottom),
+            constraints: BoxConstraints(
+                minHeight: MediaQuery.of(context).size.height * 0.8),
+            child: child,
+          ), // your column
+        );
+      },
+    );
+  }
+}
+
