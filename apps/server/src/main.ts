9d93b8db82ce92a44d25084a8cd8d99872ff91d9
Melvin <melvinnnamani@gmail.com>
2023-12-13 18:13:24 +0100

    Add morgan logging middleware to the server

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -1,5 +1,8 @@
 import express from 'express';
 import { qrCodeRoutes, redirectRoutes } from './routes/index.routes';
+import morgan from 'morgan';
+
+
 
 const host = process.env.HOST ?? 'localhost';
 const port = process.env.PORT ? Number(process.env.PORT) : 3000;
@@ -10,10 +13,11 @@ const app = express();
 app.set('trust proxy', true);
 app.use(express.json());
 
+app.use(morgan('combined'));
 
 
 app.get('/', (req, res) => {
-  res.send({ message: "zWelcome to QRL Pixel API" });
+  res.send({ message: "Welcome to QRL Pixel API" });
 });
 
 app.use(ROUTE_BASE('qrcode'), qrCodeRoutes);
