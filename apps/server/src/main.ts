219e5e951b7917e27749a1d1f3171724e9339797
Melvin <melvinnnamani@gmail.com>
2023-12-16 00:21:28 +0100

    Update express.json and express.urlencoded options

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -11,8 +11,8 @@ const ROUTE_BASE = (path: string)=> `/api/${path}`;
 const app = express();
 
 app.set('trust proxy', true);
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({limit: '50mb'}));
+app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 
 app.use(morgan('combined'));
 
