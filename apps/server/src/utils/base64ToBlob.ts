c85ac6a640287623c4f815d1274e8383840b772f
Melvin <melvinnnamani@gmail.com>
2024-04-11 00:32:30 +0100

    Add support for creating rounded image blobs

--- apps/server/src/utils/base64ToBlob.ts
+++ apps/server/src/utils/base64ToBlob.ts
@@ -1,4 +1,6 @@
-export default function base64toBlob(
+import sharp from 'sharp';
+
+export default async function base64toBlob(
   b64Data: string,
   contentType?: string,
   sliceSize = 512
@@ -14,7 +16,12 @@ export default function base64toBlob(
     contentType = 'image/png';
   }
 
-  const byteCharacters = atob(b64Data);
+  const roundedImage = await createRoundedImageBlob(
+    Buffer.from(b64Data, 'base64'),
+    8
+  );
+
+  const byteCharacters = atob(roundedImage.toString('base64'));
   const byteArrays = Array.from(
     { length: Math.ceil(byteCharacters.length / sliceSize) },
     (_, index) => {
@@ -26,4 +33,53 @@ export default function base64toBlob(
   );
 
   return new Blob(byteArrays, { type: contentType });
+}
+
+async function createRoundedImageBlob(
+  imageBuffer: Buffer,
+  radius: number
+): Promise<Buffer> {
+  const image = sharp(imageBuffer);
+  // const metadata = await image.metadata();
+
+  // const width = metadata.width;
+  // const height = metadata.height;
+
+  const mask = Buffer.from(
+    `
+  <svg xmlns="http://www.w3.org/2000/svg" width="204" height="204">
+    <g>
+      <rect
+        transform="rotate(45 102 102)"
+        rx="${radius ?? 8}"
+        height="135"
+        width="135"
+        y="34.5"
+        x="34.5"
+      />
+    </g>
+  </svg>
+  `
+  );
+
+  // Create a rounded rectangle mask with transparent background
+  // const maskBuffer = await sharp({
+  //   create: {
+  //     width,
+  //     height,
+  //     channels: 4, // RGBA for transparency
+  //     background: { r: 0, g: 0, b: 0, alpha: 0 }, // Transparent background
+  //   },
+  // });
+
+  // Apply the mask to the original image
+  return image
+    .composite([
+      {
+        input: mask,
+        blend: 'dest-in',
+      },
+    ])
+    .png()
+    .toBuffer();
 }
